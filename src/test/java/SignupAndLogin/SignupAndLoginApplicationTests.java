98e6c3b509ea2eaf435e9234ccd3ac952e2af3d2
Isha Singh <isha080400@gmail.com>
2022-09-23 16:52:56 +0530

    First Commit

---
+++ src/test/java/SignupAndLogin/SignupAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package SignupAndLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
